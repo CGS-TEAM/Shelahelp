73094d1d9f01ccbca6c4dc5376f5d6d71e1b4766
szsupunma <86388269+szsupunma@users.noreply.github.com>
2021-12-30 04:38:14 -0800

    Update __init__.py

--- AnkiVector/__init__.py
+++ AnkiVector/__init__.py
@@ -90,7 +90,7 @@ if ENV:
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-
+    uchannel = os.environ.get("uchannel", "CGSUpdates")
 
 	
     try:
