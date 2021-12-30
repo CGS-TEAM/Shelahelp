2695f1ada445e25f0f424ce0c65d12992aa9f96b
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2021-12-30 20:38:52 +0530

    Create carbon.py

---
+++ AnkiVector/modules/carbon.py
@@ -0,0 +1,48 @@
+from pyrogram import filters
+from io import BytesIO
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from AnkiVector import app, aiohttpsession as session
+from AnkiVector.fsub import ForceSub
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with session.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+BUTTON = InlineKeyboardMarkup(
+      [
+        [
+        InlineKeyboardButton(text="Join Bot Updates 🌷", url=f"https://t.me/CGSUpdates") 
+        ],
+        [
+         InlineKeyboardButton(text="➕ADD TO YOUR GROUP➕", url=f"http://t.me/TheSheLaBot?startgroup=botstart") 
+        ]
+      ]      
+    )
+
+TEXT=f"""
+Carbon create successfuly ✅✅
+
+❋ ▱▱▰▰▱▱▰▰▱▱▰▰▱▱ ❋
+Powered By : [SheLa Bot](https://t.me/TheSheLaBot)
+❋ ▱▱▰▰▱▱▰▰▱▱▰▰▱▱ ❋
+"""
+
+
+@app.on_message(filters.command(["carbon", f"carbon@TheSheLaBot"]))
+async def carbon_func(client, message):
+    FSub = await ForceSub(client, message)
+    if FSub == 400:
+        return
+    if not message.reply_to_message:
+        return await message.reply_text("Reply to a text message.")
+    if not message.reply_to_message.text:
+        return await message.reply_text("Reply to a text message.")
+    m = await message.reply_text("Preparing")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("Uploading")
+    await client.send_photo(message.chat.id, carbon,caption=TEXT,reply_markup= BUTTON)
+    await m.delete()
+    carbon.close()
