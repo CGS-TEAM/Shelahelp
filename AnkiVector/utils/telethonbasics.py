1f0b107b68f39b3438d8b4d738ae50a53f7277d6
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2021-12-31 16:07:54 +0530

    Update telethonbasics.py

--- AnkiVector/utils/telethonbasics.py
+++ AnkiVector/utils/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from SheLaBot.function.pluginhelpers import runcmd
+from AnkiVector.utils.pluginhelpers import runcmd
 
 
 async def convert_to_image(event, borg):
