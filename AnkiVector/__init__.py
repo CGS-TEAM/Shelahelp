717b57ec2e7cab2302dd297bc7e568a16c0b694f
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2021-12-31 19:35:34 +0530

    Update __init__.py

--- AnkiVector/__init__.py
+++ AnkiVector/__init__.py
@@ -7,21 +7,7 @@ import time
 import spamwatch
 import telegram.ext as tg
 from pyrogram import Client, errors
-from telethon import TelegramClient
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
 
-
-def start() -> scoped_session:
-    engine = create_engine(POSTGRESS_URL, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-BASE = declarative_base()
-SESSION = start()
 
 StartTime = time.time()
 aiohttpsession = aiohttp.ClientSession()
