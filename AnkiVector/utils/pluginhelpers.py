e8842de6ece84b8caf772ff9b34be07cdf63cc31
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2021-12-31 16:04:11 +0530

    Update pluginhelpers.py

--- AnkiVector/utils/pluginhelpers.py
+++ AnkiVector/utils/pluginhelpers.py
@@ -13,8 +13,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from SheLaBot import OWNER_ID, SUPPORT_CHAT
-from SheLaBot import pbot
+from AnkiVector import OWNER_ID, SUPPORT_CHAT
+from AnkiVector import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
