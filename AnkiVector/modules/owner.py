cc1a850a9caaa05c9631a19b47909c2224e9499c
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-01 16:41:22 +0530

    Update owner.py

--- AnkiVector/modules/owner.py
+++ AnkiVector/modules/owner.py
@@ -1,6 +1,7 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from AnkiVector import pbot
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import idle, filters
 
 OWNER_TEXT = """
