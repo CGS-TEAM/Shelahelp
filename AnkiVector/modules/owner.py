df1220f0ff5bf2f16c68854e3c7b112fba564085
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-01 16:37:09 +0530

    Update owner.py

--- AnkiVector/modules/owner.py
+++ AnkiVector/modules/owner.py
@@ -8,6 +8,6 @@ OWNER_TEXT = """
 """
 
 @pbot.on_message(filters.regex("@kmsrk") & ~filters.private)
-async def owner(client, message):
+async def owner(_, message: Message):
     await message.reply_text(
                     caption=OWNER_TEXT)
