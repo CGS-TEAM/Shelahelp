55515101998139cc21622d5c97187832ace7efa0
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-03 12:44:46 +0530

    Update dbfunctions.py

--- AnkiVector/utils/dbfunctions.py
+++ AnkiVector/utils/dbfunctions.py
@@ -25,7 +25,6 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from AnkiVector import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
