39d586e82c0d57a3fd6172d8a329bd09bf36f452
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-03 21:18:44 +0530

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -62,6 +62,8 @@ from AnkiVector.modules.helper_funcs.alternate import typing_action
 from AnkiVector.modules.helper_funcs.chat_status import is_user_admin
 from AnkiVector.modules.helper_funcs.misc import paginate_modules
 from AnkiVector.modules.helper_funcs.readable_time import get_readable_time
+from AnkiVector.modules.system_stats import bot_sys_stats
+
 
 PM_START_TEXT = f"""
 *✨Hey There i am SheLa Bot.*
