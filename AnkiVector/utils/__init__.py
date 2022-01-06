ae0c9f08b9d2bb8c201eb18db215173af01219dd
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-06 11:09:36 +0530

    Update __init__.py

--- AnkiVector/utils/__init__.py
+++ AnkiVector/utils/__init__.py
@@ -1,2 +1,11 @@
 from .progress import progress
+from AnkiVector import MONGO_DB_URI
 from .tools import human_to_bytes, humanbytes, md5, time_formatter
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+
+
+
+# MongoDB client
+print("[INFO]: INITIALIZING DATABASE")
+mongo_client = MongoClient(MONGO_DB_URI)
+db = mongo_client.AnkiVector
