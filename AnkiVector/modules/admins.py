de2c53dc68ff6add2498dea55dc5ca8a1dc568a5
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-06 12:56:16 +0530

    Update admins.py

--- AnkiVector/modules/admins.py
+++ AnkiVector/modules/admins.py
@@ -86,7 +86,7 @@ async def member_permissions(chat_id: int, user_id: int):
     return perms
 
 
-from wbb.core.decorators.permissions import adminsOnly
+from AnkiVector.core.decorators.permissions import adminsOnly
 
 
 async def list_admins(chat_id: int):
