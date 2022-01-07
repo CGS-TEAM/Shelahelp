1fb1b28b54d39baed78a8687e7e11dd5d242b91a
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-07 21:37:06 +0530

    Update banss.py

--- AnkiVector/modules/banss.py
+++ AnkiVector/modules/banss.py
@@ -3,6 +3,9 @@ __help__ = """
 /dban - Delete the replied message banning its sender
 /tban - Ban A User For Specific Time
 /unban - Unban A User
+/kick - Kick A User
+/dkick - Delete the replied message kicking its sender
+/banghosts - Ban Deleted Accounts
  
  @TheSheLaBot
 """
