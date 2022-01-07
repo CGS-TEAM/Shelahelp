de5253968434479bfd3c375a6711e5e03a2dd0a8
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-07 21:35:35 +0530

    Create wrn.py

---
+++ AnkiVector/modules/wrn.py
@@ -0,0 +1,13 @@
+__help__ = """
+
+**Admins  **
+/warn - Warn A User
+/dwarn - Delete the replied message warning its sender
+/rmwarns - Remove All Warning of A User
+/warns - Show Warning Of A User
+ 
+ @TheSheLaBot
+
+"""
+
+__mod_name__ = "Mutes"
