83a4ddd8afa279bc6b90ab57dd372b79809566af
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-07 21:30:37 +0530

    Create banss.py

---
+++ AnkiVector/modules/banss.py
@@ -0,0 +1,11 @@
+__help__ = """
+**Admins  **
+/dban - Delete the replied message banning its sender
+/tban - Ban A User For Specific Time
+/unban - Unban A User
+/warn - Warn A User
+/dwarn - Delete the replied message warning its sender
+ 
+ @TheSheLaBot
+"""
+__mod_name__ = "Bans"
