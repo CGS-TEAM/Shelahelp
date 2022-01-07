d7eeda24552719d9999c1b2118ea2bfdf23b11b3
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-07 21:06:05 +0530

    Create admins.py

---
+++ AnkiVector/modules/admins.py
@@ -0,0 +1,13 @@
+__help__ = """
+**Admins  **
+/promote - Promote A Member
+/fullpromote - Promote A Member With All Rights
+/demote - Demote A Member
+/pin - Pin A Message
+/unpin - Unpin A message 
+/zombies clear - Delete account remove
+
+ 
+ @TheSheLaBot
+"""
+__mod_name__ = "Book"
