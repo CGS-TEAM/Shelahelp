3f85e2ac95dc08490142aa67c2981ce5688ca522
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-07 21:31:18 +0530

    Update banss.py

--- AnkiVector/modules/banss.py
+++ AnkiVector/modules/banss.py
@@ -3,8 +3,6 @@ __help__ = """
 /dban - Delete the replied message banning its sender
 /tban - Ban A User For Specific Time
 /unban - Unban A User
-/warn - Warn A User
-/dwarn - Delete the replied message warning its sender
  
  @TheSheLaBot
 """
