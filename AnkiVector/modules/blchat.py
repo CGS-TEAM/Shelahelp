16b315f0db3cfde41d4116f016bd884b55e4318b
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-07 21:55:06 +0530

    Add files via upload

---
+++ AnkiVector/modules/blchat.py
@@ -0,0 +1,9 @@
+__help__ = """
+**Admins  **
+/blacklisted - Get All The Blacklisted Words In The Chat.
+/blacklist [WORD|SENTENCE] - Blacklist A Word Or A Sentence.
+/whitelist [WORD|SENTENCE] - Whitelist A Word Or A Sentence.
+
+ @TheSheLaBot
+"""
+__mod_name__ = "Blacklist"
