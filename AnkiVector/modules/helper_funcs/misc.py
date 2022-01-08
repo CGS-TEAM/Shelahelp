b062e219c610ba74cb812ee030f6bb8b9e42297d
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-08 11:15:00 +0530

    Update misc.py

--- AnkiVector/modules/helper_funcs/misc.py
+++ AnkiVector/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 9:
         pairs = pairs[modulo_page * 9:9 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton(⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🔙 Back", callback_data="helpmenu_"),
+                EqInlineKeyboardButton("🔙 Back", callback_data="aboutmanu_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🔙 Back", callback_data="helpmenu_")]]
+        pairs += [[EqInlineKeyboardButton("🔙 Back", callback_data="aboutmanu_back")]]
 
     return pairs
 
