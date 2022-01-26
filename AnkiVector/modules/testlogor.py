4b20b4b1f4d3b0a2ec3641f426542f959d7fc81a
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-26 21:46:38 +0530

    Update testlogor.py

--- AnkiVector/modules/testlogor.py
+++ AnkiVector/modules/testlogor.py
@@ -25,12 +25,11 @@ def get_text(message: Message) -> [None, str]:
 
 @pbot.on_message(filters.command(["sdlogo"]))
 async def logo(bot, update):
+        await update.reply_text("⚙️ Creating logo plz wait..")
     try:      
         name = update.text.split(None, 1)[1]
         req = requests.get(f"https://sd-logo-api.herokuapp.com/?logo={name}")
         IMG = req.text
-        await update.reply_text("⚙️ Creating logo plz wait..")
-        return
         await update.reply_photo(photo = IMG, caption = caption) 
     except Exception as e:
         await update.reply_text(f"Error: {e}")
