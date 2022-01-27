7b4cca96fa56332391d01ad8b359376f6aa22ef5
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-27 19:01:15 +0530

    Update testlogor.py

--- AnkiVector/modules/testlogor.py
+++ AnkiVector/modules/testlogor.py
@@ -25,6 +25,8 @@ def get_text(message: Message) -> [None, str]:
 
 @pbot.on_message(filters.command(["sdlogo"]))
 async def logo(bot, update):
+    FSub = await ForceSub(client, message)
+    if FSub == 400:
     quew = get_text(update)
     if not quew:
         await update.reply_text(message.chat.id, "😶Please give a text handwrite.")
