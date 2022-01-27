28ffe4b9df61b693bab17a2ce4f408c44cfe2d3e
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-27 22:05:48 +0530

    Update carbon.py

--- AnkiVector/modules/carbon.py
+++ AnkiVector/modules/carbon.py
@@ -1,29 +1,9 @@
-import os
-import io
-import requests
-from bs4 import *	
-from PIL import Image
-from AnkiVector import pbot
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
-from requests import get
-from pyrogram.types import Message
-from pyrogram import filters, Client
+from pyrogram import filters
 from io import BytesIO
-from AnkiVector import aiohttpsession as session
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from AnkiVector import pbot, aiohttpsession as session
 from AnkiVector.fsub import ForceSub
 
-def get_text(message: Message) -> [None, str]:
-    text_to_return = message.text
-    if message.text is None:
-        return None
-    if " " in text_to_return:
-        try:
-            return message.text.split(None, 1)[1]
-        except IndexError:
-            return None
-    else:
-        return None
-      
 async def make_carbon(code):
     url = "https://carbonara.vercel.app/api/cook"
     async with session.post(url, json={"code": code}) as resp:
@@ -31,7 +11,6 @@ async def make_carbon(code):
     image.name = "carbon.png"
     return image
 
-name = update.text.split(None, 1)[1]
 BUTTON = InlineKeyboardMarkup(
       [
         [
@@ -57,12 +36,12 @@ async def carbon_func(client, message):
     FSub = await ForceSub(client, message)
     if FSub == 400:
         return
-    if not message.reply_text:
-        return await message.reply_text("give a name.")
-    if not message.reply_text:
+    if not message.reply_to_message:
         return await message.reply_text("Reply to a text message.")
-    m = await message.reply_text("⚙️ Please wait creating carbon..")
-    carbon = await make_carbon, {name}
+    if not message.reply_to_message.text:
+        return await message.reply_text("Reply to a text message.")
+    m = await message.reply_text("Preparing")
+    carbon = await make_carbon(message.reply_to_message.text)
     await m.edit("Uploading")
     await client.send_photo(message.chat.id, carbon,caption=TEXT,reply_markup= BUTTON)
     await m.delete()
