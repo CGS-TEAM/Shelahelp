25287aa51e5372a7dd7ac2aa8427138900bff9c3
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-29 21:06:33 +0530

    Create filterss.py

---
+++ AnkiVector/modules/filterss.py
@@ -0,0 +1,10 @@
+_mod_name_ = "Filters"
+__help__ = """
+/filters To Get All The Filters In The Chat.
+/filter [FILTER_NAME] To Save A Filter (Can be a sticker or text).
+/stop [FILTER_NAME] To Stop A Filter.
+
+You can use markdown or html to save text too.
+
+Checkout /markdownhelp to know more about formattings and other syntax.
+"""
